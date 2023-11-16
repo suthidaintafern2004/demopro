b05e21b26f487239c43fb18dcd9fbce7df59808d
sithidaintafern2004 <suthidainta7@gmail.com>
2023-11-16 09:31:40 +0700

    first commit

---
+++ bin/demopro.dart
@@ -0,0 +1,5 @@
+import 'package:demopro/demopro.dart' as demopro;
+
+void main(List<String> arguments) {
+  print('Hello world: ${demopro.calculate()}!');
+}
---
+++ test/demopro_test.dart
@@ -0,0 +1,8 @@
+import 'package:demopro/demopro.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
